d91ffa22ecc164e6f4b4f18f7c80c78f7f1c1c84
dashpen <dashpenning@gmail.com>
2022-06-24 22:58:13 -0700

    good message

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'sus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -75,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Container(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -93,8 +94,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+           
             const Text(
               'You have pushed the button this many times amogus:',
             ),
@@ -102,6 +104,8 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            SizedBox(height: 100, width: 100,),
+             
           ],
         ),
       ),
---
+++ lib/sus.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Amogus extends StatelessWidget {
+  const Amogus({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("sus"));
+  }
+}
